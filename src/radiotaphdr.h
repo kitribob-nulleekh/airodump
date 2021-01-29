7e5542b6dc19a5860efe8779ad20b73fae4bfbda
nulLeeKH <i_am@nulleekh.com>
2021-01-29 16:31:42 +0900

    add: src/radiotaphdr.h

---
+++ src/radiotaphdr.h
@@ -0,0 +1,20 @@
+#ifndef SRC_RADIOTAPHDR_H
+#define SRC_RADIOTAPHDR_H
+
+#define RADIOTAP_SIZE 18
+
+struct radHdr {
+    uint8_t res;
+    uint8_t pad;
+    uint16_t len;
+    uint32_t preFlag;
+    uint8_t flag;
+    uint8_t rate;
+    uint16_t freq;
+    uint16_t chnFlag;
+    uint8_t atnSig;
+    uint8_t atn;
+    uint16_t rFlag;
+};
+
+#endif //SRC_RADIOTAPHDR_H
